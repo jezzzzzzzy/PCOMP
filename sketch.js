0f05e67677439ae43e6766db8d0d088c6aaf0ca1
jezzy <jezzzy@10-18-130-159.dynapool.wireless.nyu.edu>
2020-10-27 17:42:55 -0400

    another change

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function setup() {
 
 function draw(){
   background(230);
-  // hello i am changing something else now and something else
+  // hello i am changing something else now and something else asdflkjas;dlfkj
   textHere();
 }
 
